b787f307aaf22a649d2b2c19ac95de053551ae26
Kyle Morton <kylemortondevelopment@gmail.com>
2020-08-27 13:31:31 -0500

    working on adding blog posts page and forms

---
+++ BlazorCMS/BlazorCMS/Web/Pages/PostsBase.cs
@@ -0,0 +1,33 @@
+﻿using BlazorCMS.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorCMS.Web.Pages
+{
+    public class PostsBase : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+        [Parameter]
+        public string BlogId { get; set; }
+
+        protected List<BlogPost> Posts { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (string.IsNullOrEmpty(BlogId))
+            {
+                NavigationManager.NavigateTo("/blogs");
+                return null;
+            }
+
+            Posts = await Http.GetFromJsonAsync<List<BlogPost>>($"Post/{BlogId}");
+        }
+    }
+}
